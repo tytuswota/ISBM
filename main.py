63f6f9531179715fbeb0df45b0991df11f3b8785
denizik12 <denizik12@hotmail.com>
2020-10-25 21:58:26 +0100

    clean code

--- main.py
+++ main.py
@@ -4,35 +4,51 @@ from geopy.geocoders import Nominatim
 from datetime import datetime
 
 #get gps data
-geolocator = Nominatim(user_agent="openstreetmap")
-location = geolocator.geocode("Spijkenisse")
-latitude = location.latitude
-longitude = location.longitude
-data_gps = "1"
+def get_gps(self):
+    geolocator = Nominatim(user_agent="openstreetmap")
+    location = geolocator.geocode("Spijkenisse")
+    latitude = location.latitude
+    longitude = location.longitude
+    data_gps = 1
+    return data_gps, latitude, longitude
 
 #gps data to one string
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-string_value = str(data_gps) + ";" + str(latitude) + ";" + str(longitude) + ";" + dt_string
-#print(string_value)
+def get_string(data_gps, latitude, longitude):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    string_value = str(data_gps) + ";" + str(latitude) + ";" + str(longitude) + ";" + dt_string
+    return string_value
 
 #format string to ascii
-ascii_value = ' '.join(list(map(str,map(ord,string_value))))
-#print(ascii_value)
+def get_ascii(string_value):
+    ascii_value = ' '.join(list(map(str,map(ord,string_value))))
+    return ascii_value
 
 #format string to binary
-binary_value = ' '.join(format(ord(x), 'b') for x in string_value)
+def get_binary(string_value):
+    binary_value = ' '.join(format(ord(x), 'b') for x in string_value)
+    return binary_value
 
 #format string to hex
-# string_to_bytes = bytes(string_value, 'utf-8')
-# hex_value = codecs.encode(string_to_bytes, "hex")
-# hex_server = hex_value.decode()
-# print(hex_server)
+def get_hex(string_value):
+    string_to_bytes = bytes(string_value, 'utf-8')
+    hex_value = codecs.encode(string_to_bytes, "hex")
+    hex_server = hex_value.decode()
+    return hex_server
 
-url = 'http://145.24.222.137/index.php?incoming_message='+ binary_value
-x = requests.get(url)
+def get_url(binary_value):
+    url = 'http://145.24.222.137/index.php?incoming_message='+ binary_value
+    x = requests.get(url)
+    #print(x.text)
+    return x
 
-print(x.text)
+data_type, latitude, longitude = get_gps(self="")
 
-#print the response text (the content of the requested file):
-#print(x.text)
+data_string = get_string(data_type, latitude, longitude)
+#print(data_string)
+
+binary_data = get_binary(data_string)
+#print(binary_data)
+
+url = get_url(binary_data)
+#print(url)
