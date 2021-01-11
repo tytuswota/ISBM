530d12a8da8011f56c5d2172db9de5e860447053
joshua asmowidjojo <jojo.asmo94@gmail.com>
2021-01-11 18:32:39 +0100

    de .ino file van iridiumSendReceive file onderverdeeld in header file en daar library van gemaakt..

---
+++ IridiumSendRecieveMsgGps/Message/Message.h
@@ -0,0 +1,27 @@
+#ifndef Message_h
+#define Message_h
+
+class messageclass
+{
+  public:
+    messageclass();
+    void SETUP();
+    void printFirmwareRevision();
+    String intToHex(int myInt);
+    String floatToHex(float myFloat);
+    String stringToHex(String myString);
+    char* string2char(String command);
+    void SERCOM1_Handler();
+    void sendMessage(String msg);
+    void getMessage();
+    void getGPSData();
+    void getSignal();
+    float lattitude, longitude; 
+    int signalQuality = -1;
+    int err;
+    bool orders = false;
+};
+
+extern messageclass mess;
+
+#endif
---
+++ IridiumSendRecieveMsgGps/README.md.txt
@@ -0,0 +1,5 @@
+COPY MESSAGE FOLDER TO YOUR ARDUINO LIBRARY FOLDER
+
+eg:
+
+C:\Users\<user>\Documents\Arduino\libraries
