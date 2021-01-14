4c969afe8d004e9e9cea1a6a6ab9a6722923b058
tytuswota <wabijack@gmail.com>
2021-01-14 20:45:53 +0100

    Communication controller werk nu met platform io

---
+++ CommunicationController/src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include "message.h"
+
+void setup() {
+  msg.SETUP(); 
+  msg.printFirmwareRevision();
+}
+
+void loop() 
+{
+  if(!msg.orders)
+  {
+    SerialUSB.println("Waiting for orders...");
+    SerialUSB.println("give user input!");
+    msg.orders = true;
+  }
+
+  if(SerialUSB.available() > 0)
+  {
+    String input = SerialUSB.readString();
+    input.trim();
+
+    if((input == "get signal") || (input == "sig"))   // 'get signal' to request signal strength from ISBD module
+    {
+      SerialUSB.println("user input:" + input);
+      msg.getSignal();
+      msg.orders = false;
+    }
+    
+    if((input == "gps"))                              // 'gps' to request current longitude and lattitude from GPS module
+    {
+      SerialUSB.println("user input:" + input);
+      msg.getGPSData();
+      msg.orders = false;
+    }
+    
+    if((input == "get msg"))                          // 'get msg' to download the first MT (Mobile Terminated) message to ISBD module *REQUIRES LINE-OF-SIGHT VIEW TO SATELLITE*
+    {
+      SerialUSB.println("user input:" + input);
+      msg.getMessage();
+      msg.orders = false;
+    }
+    
+    if((input == "send gps msg"))                     // 'send gps msg' to send MO (Mobile Originated) message to Iridium Gateway *REQUIRES LINE-OF-SIGHT VIEW TO SATELLITE*
+    {
+      SerialUSB.println("user input:" + input);
+      msg.getGPSData();
+      String message = "1;" + String(msg.lattitude,6) + ";" + String(msg.longitude,6);
+      msg.sendMessage(message);
+      msg.orders = false;
+    }
+  }
+}
---
+++ CommunicationController/src/message.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#ifndef Message_h
+#define Message_h
+
+class Message
+{
+  public:
+    Message();
+    void SETUP();
+    void printFirmwareRevision();
+    String intToHex(int myInt);
+    String floatToHex(float myFloat);
+    String stringToHex(String myString);
+    char* string2char(String command);
+    void SERCOM1_Handler();
+    void sendMessage(String msg);
+    void getMessage();
+    void getGPSData();
+    void getSignal();
+    float lattitude, longitude; 
+    int signalQuality = -1;
+    int err;
+    bool orders = false;
+};
+
+extern Message msg;
+
+#endif
